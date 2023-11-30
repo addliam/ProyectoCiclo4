7d0087d2820f9d2af64eb2d073c465e819fbb4be
xfab1an1 <xfabianyp22@gmail.com>
2023-11-29 19:30:42 -0500

    arreglarRespuesta

--- src/main/java/com/java/proyectociclo4/entity/Respuesta.java
+++ src/main/java/com/java/proyectociclo4/entity/Respuesta.java
@@ -10,47 +10,15 @@ package com.java.proyectociclo4.entity;
  */
 public class Respuesta {
 
-    private String respuestaid;
-    private String usuarioEmail;
-    private Integer formularioId;
+   private Integer formularioId;
     private String contenido;
     private Integer categoriaId;
+    private String usuarioEmail;
 
-    public Respuesta(String Respuestaid, String usuarioEmail, Integer formularioId, String contenido, Integer categoriaId) {
-        this.respuestaid = Respuestaid;
-        this.usuarioEmail = usuarioEmail;
+    public Respuesta(Integer formularioId, String contenido, Integer categoriaId, String usuarioEmail) {
         this.formularioId = formularioId;
         this.contenido = contenido;
         this.categoriaId = categoriaId;
-    }
-
-    @Override
-    public String toString() {
-        return "respuestaid:" + respuestaid + "\nusuarioEmail:" + usuarioEmail + "\nformularioId:" + formularioId
-                + "\ncontenido:" + contenido + "\ncategoriaId:" + categoriaId;
-    }
-
-    public Integer getCategoriaId() {
-        return categoriaId;
-    }
-
-    public void setCategoriaId(Integer categoriaId) {
-        this.categoriaId = categoriaId;
-    }
-
-    public String getRespuestaid() {
-        return respuestaid;
-    }
-
-    public void setRespuestaid(String Respuestaid) {
-        this.respuestaid = Respuestaid;
-    }
-
-    public String getUsuarioEmail() {
-        return usuarioEmail;
-    }
-
-    public void setUsuarioEmail(String usuarioEmail) {
         this.usuarioEmail = usuarioEmail;
     }
 
@@ -69,5 +37,22 @@ public class Respuesta {
     public void setContenido(String contenido) {
         this.contenido = contenido;
     }
+
+    public Integer getCategoriaId() {
+        return categoriaId;
+    }
+
+    public void setCategoriaId(Integer categoriaId) {
+        this.categoriaId = categoriaId;
+    }
+
+    public String getUsuarioEmail() {
+        return usuarioEmail;
+    }
+
+    public void setUsuarioEmail(String usuarioEmail) {
+        this.usuarioEmail = usuarioEmail;
+    }
+
 
 }
